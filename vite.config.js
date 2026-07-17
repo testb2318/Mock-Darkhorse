47c66cf7601c42cf9ad6fcf33a81a3c499b974c2
testb2318 <testb2318@gmail.com>
2026-07-17 12:52:54 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+    },
+  },
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
